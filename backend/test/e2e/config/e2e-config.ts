024808ed65f2add52e01cbfdd374c02aa6a7b1de
OREN-VIVOBOOK\oren1 <oren1407@gmail.com>
2026-04-10 17:22:59 +0300

    remove unused files

--- backend/test/e2e/config/e2e-config.ts
+++
@@ -1,6 +0,0 @@
-import { Config } from '@src/config/config';
-
-export const e2eConfig = () => Config.create().getConfig();
-
-// Extend config here if needed
-// export const e2eConfig = () => ({ ...loadE2EConfig(), customE2EProperty: 'value' });
--- backend/test/unit/config/unit-config.ts
+++
@@ -1,9 +0,0 @@
-import { Config } from '@src/config/config';
-
-export const unitConfig = () => Config.create(
-	{},
-	{ PROTOCOL: 'mongodb', USER: 'mock', PASSWORD: 'mock', HOST: 'localhost', DB_NAME: 'mockdb' }
-).getConfig();
-
-// Extend config here if needed
-// export const unitConfig = () => ({ ...unitConfig(), customUnitProperty: 'value' });
